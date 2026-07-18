12da19b566989fbe8dc202762363c6ad213b064e
slimckay <kpangesamuel84@gmail.com>
2026-07-18 20:02:48 +0000

    Hero image optimized 850px - PHP assembler for 4 parts

--- images/hero-serve.php
+++ images/hero-serve.php
@@ -1,9 +1,6 @@
 <?php
 header('Content-Type: image/jpeg');
 header('Cache-Control: public, max-age=604800');
-$b64 = '';
-for ($i = 1; $i <= 12; $i++) {
-    $f = __DIR__ . "/h{$i}.txt";
-    if (file_exists($f)) $b64 .= trim(file_get_contents($f));
-}
+$b64='';
+for($i=1;$i<=4;$i++){$f=__DIR__."/h$i.txt";if(file_exists($f))$b64.=trim(file_get_contents($f));}
 echo base64_decode($b64);
