8c7693395fff0ffd068d51afb019e1dfa3210973
slimckay <kpangesamuel84@gmail.com>
2026-07-18 20:09:11 +0000

    Update hero image assembler for 11 high-res parts

--- images/hero-serve.php
+++ images/hero-serve.php
@@ -1,6 +1,9 @@
 <?php
 header('Content-Type: image/jpeg');
 header('Cache-Control: public, max-age=604800');
-$b64='';
-for($i=1;$i<=4;$i++){$f=__DIR__."/h$i.txt";if(file_exists($f))$b64.=trim(file_get_contents($f));}
+$b64 = '';
+for ($i = 1; $i <= 11; $i++) {
+    $f = __DIR__ . "/h{$i}.txt";
+    if (file_exists($f)) $b64 .= trim(file_get_contents($f));
+}
 echo base64_decode($b64);
