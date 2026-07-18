c98651afe3a98490e3c43904318fca15aa22f54c
slimckay <kpangesamuel84@gmail.com>
2026-07-18 23:59:42 +0000

    Load phone notifications from main.js; inject boot on messages via inline footer patch file

---
+++ js/main-notify-append.js
@@ -0,0 +1,7 @@
+/* Include after main.js on patient dashboard if needed */
+(function(){
+  var s=document.createElement('script');
+  s.src='../js/push-notify.js';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
